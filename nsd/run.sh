83747d74fc46e2c91536315bbd1e4fa8ebf715e2
Drewshg312 <drewshg@gmail.com>
2020-09-22 14:31:03 +0300

    updated to v4.3.2
    multiple vlan zone setup

--- nsd/run.sh
+++ nsd/run.sh
@@ -1,16 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "${NSD_BASE64_CFG}" | base64 -d > /etc/nsd/nsd.conf
-echo "${NSD_BASE64_ZONE_CFG}" | base64 -d > "/zones/${NSD_ZONE}.zone"
-echo "${NSD_BASE64_REV_ZONE_CFG}" | base64 -d > "/zones/${NSD_REV_ZONE}.zone"
+nsd_cfg='/etc/nsd/nsd.conf'
+zones_path='/zones'
+sed_path='/bin/sed'
 
-/bin/sed -i "s/NSD_ZONE/${NSD_ZONE}/" /etc/nsd/nsd.conf
-/bin/sed -i "s/NSD_REV_ZONE/${NSD_REV_ZONE}/" /etc/nsd/nsd.conf
+# Generate default nsd.config template:
+echo "${NSD_BASE64_CFG}" | base64 -d > "${nsd_cfg}"
 
+# Configure DNS zones:
+for i in 0 1 2 3
+do
+    zone="NSD_ZONE_${i}"
+    zone_cfg="NSD_BASE64_ZONE_${i}_CFG"
+    zone_signed="NSD_ZONE_${i}_SIGNED"
+
+    # Create zone config files:
+    echo "${!zone_cfg}" | base64 -d > "${zones_path}/${!zone}.zone"
+
+    # Specify correct zone configs in nsd.conf:
+    if [ "${!zone_signed}" == true ]; then
+        "${sed_path}" -i "s/\(${zone}.zone\)/\1.signed/" "${nsd_cfg}"
+    fi
+    "${sed_path}" -i "s/${zone}/${!zone}/" "${nsd_cfg}"
+done
+
+# Start NSD:
 if [ ! -f /etc/nsd/nsd_server.pem ]; then
   nsd-control-setup
 fi
-
 chown -R $UID:$GID /var/db/nsd/ /etc/nsd /tmp
-
 exec /sbin/tini -- nsd -u $UID.$GID -P /tmp/nsd.pid -d
